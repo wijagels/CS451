ab42c648e454713caee93bf736179e2a1ca2e51c
William Jagels <will.jagels@gmail.com>
2016-11-16 13:54:07 -0500

    Clean up the base code

--- proj2/common.h
+++ proj2/common.h
@@ -3,32 +3,42 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-#define SUCCESS  0
-#define ERROR    -1
+/* This snippet enables the use of __func__ instead of the less-portable
+ * __FUNCTION__. This allows me to turn on -pedantic-errors.
+ * https://gcc.gnu.org/onlinedocs/gcc-4.5.3/gcc/Function-Names.html */
+#if __STDC_VERSION__ < 199901L
+#if __GNUC__ >= 2
+#define __func__ __FUNCTION__
+#else
+#define __func__ "<unknown>"
+#endif
+#endif
+
+#define SUCCESS 0
+#define ERROR -1
 
 #define US_PER_SEC 1000000
 
 #define DEBUG
 #ifdef DEBUG
-#define DEBUG_MSG(fmt, args...) printf("%s(): \t"fmt, __FUNCTION__, ##args)
+#define DEBUG_MSG(fmt, args...) printf("%s(): \t" fmt, __func__, ##args)
 #else
 #define DEBUG_MSG(fmt, args...)
 #endif
 
-#define ERR_MSG(fmt, args...) printf("ERROR in %d:%s(): "fmt, __LINE__, __FUNCTION__, ##args)
+#define ERR_MSG(fmt, args...) \
+  printf("ERROR in %d:%s(): " fmt, __LINE__, __func__, ##args)
 
 #define EXIT_ERROR(v, fmt, args...) \
-    do                              \
-    {                               \
-        ERR_MSG(fmt, ##args);       \
-        exit(v);              \
-    } while(0)
+  do {                              \
+    ERR_MSG(fmt, ##args);           \
+    exit(v);                        \
+  } while (0)
 
 #define _EXIT_ERROR(v, fmt, args...) \
-    do                               \
-    {                                \
-        ERR_MSG(fmt, ##args);        \
-        _exit(v);              \
-    } while(0)
+  do {                               \
+    ERR_MSG(fmt, ##args);            \
+    _exit(v);                        \
+  } while (0)
 
 #endif
--- proj2/usr_functions.h
+++ proj2/usr_functions.h
@@ -5,12 +5,10 @@
 
 #include "mapreduce.h"
 
+int letter_counter_map(DATA_SPLIT* split, int fd_out);
+int letter_counter_reduce(int* p_fd_in, int fd_in_num, int fd_out);
 
-int letter_counter_map(DATA_SPLIT * split, int fd_out);
-int letter_counter_reduce(int * p_fd_in, int fd_in_num, int fd_out);
-
-int word_finder_map(DATA_SPLIT * split, int fd_out);
-int word_finder_reduce(int * p_fd_in, int fd_in_num, int fd_out);
-
+int word_finder_map(DATA_SPLIT* split, int fd_out);
+int word_finder_reduce(int* p_fd_in, int fd_in_num, int fd_out);
 
 #endif
