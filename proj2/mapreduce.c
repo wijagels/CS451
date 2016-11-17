f690b5abd42a05da511dbd2d1dff8a48cac3a1da
William Jagels <will.jagels@gmail.com>
2016-11-17 00:39:30 -0500

    Map reduce works for counting

--- proj2/mapreduce.c
+++ proj2/mapreduce.c
@@ -1,14 +1,26 @@
 #include "mapreduce.h"
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "common.h"
 
-// add your code here ...
+static int* fd_arr;
+static pid_t* pid_arr;
+static pthread_mutex_t reduce_lock = PTHREAD_MUTEX_INITIALIZER;
+
+void worker(MAPREDUCE_SPEC* spec, DATA_SPLIT* ds, int fd,
+            char special_snowflake);
 
 void mapreduce(MAPREDUCE_SPEC* spec, MAPREDUCE_RESULT* result) {
-    // add you code here ...
-
     struct timeval start, end;
 
     if (NULL == spec || NULL == result) {
@@ -17,10 +29,93 @@ void mapreduce(MAPREDUCE_SPEC* spec, MAPREDUCE_RESULT* result) {
 
     gettimeofday(&start, NULL);
 
-    // add your code here ...
+    /* Get size */
+    FILE* fp = fopen(spec->input_data_filepath, "r");
+    fseek(fp, 0, SEEK_END);
+    int64_t sz = ftell(fp);
+    rewind(fp);
+
+    int64_t boundary = sz / spec->split_num;
+    char part_name[FILENAME_MAX];
+    fd_arr = malloc(spec->split_num * sizeof(int));
+    pid_arr = malloc(spec->split_num * sizeof(pid_t));
+    if (!fd_arr) EXIT_ERROR(1, "%s", "Fatal malloc error");
+    for (int i = 0; i < spec->split_num; i++) {
+        snprintf(part_name, FILENAME_MAX, "mr-%d.itm", i);
+        unlink(part_name);
+        fd_arr[i] = open(part_name, O_CREAT | O_RDWR, S_IRWXU);
+    }
+    char special_snowflake = 1;
+
+    for (int i = 0; i < spec->split_num - 1; i++) {
+        size_t part_sz = boundary;
+        fseek(fp, boundary, SEEK_CUR);
+        char c = fgetc(fp);
+        /* Stop as soon as we hit a non-letter */
+        while (isalpha(c)) {
+            ++part_sz;
+            c = fgetc(fp);
+        }
+        int64_t offset = ftell(fp) - part_sz - 1;
+        pid_t pid = fork();
+        if (!pid) {
+            int fd = open(spec->input_data_filepath, O_RDONLY);
+            lseek(fd, offset, SEEK_SET);
+            DATA_SPLIT ds = {fd, part_sz, spec->usr_data};
+            worker(spec, &ds, fd_arr[i], special_snowflake);
+            return;
+        }
+        pid_arr[i] = pid;
+        special_snowflake = 0;
+    }
+    int64_t offset = ftell(fp) - 1;
+    pid_t pid = fork();
+    if (!pid) {
+        int fd = open(spec->input_data_filepath, O_RDONLY);
+        lseek(fd, offset, SEEK_SET);
+        DATA_SPLIT ds = {fd, sz - offset, spec->usr_data};
+        worker(spec, &ds, fd_arr[spec->split_num - 1], special_snowflake);
+        return;
+    }
+    pid_arr[spec->split_num - 1] = pid;
+
+    for (int i = 1; i < spec->split_num; i++) waitpid(pid_arr[i], NULL, 0);
+    kill(pid_arr[0], SIGUSR1);
+    waitpid(pid_arr[0], NULL, 0);
 
     gettimeofday(&end, NULL);
 
     result->processing_time = (end.tv_sec - start.tv_sec) * US_PER_SEC +
                               (end.tv_usec - start.tv_usec);
+    result->map_worker_pid = pid_arr;
+    result->reduce_worker_pid = pid_arr[0];
+    result->filepath = "mr.rst";
+    free(fd_arr);
+}
+
+void sigusr1_handler(int sig) {
+    if (sig == SIGUSR1) pthread_mutex_unlock(&reduce_lock);
+}
+
+void worker(MAPREDUCE_SPEC* spec, DATA_SPLIT* ds, int fd,
+            char special_snowflake) {
+    sigset_t sigmask;
+    if (special_snowflake) {
+        sigemptyset(&sigmask);
+        sigaddset(&sigmask, SIGUSR1);
+        sigprocmask(SIG_BLOCK, &sigmask, NULL);
+
+        pthread_mutex_lock(&reduce_lock);
+        signal(SIGUSR1, &sigusr1_handler);
+    }
+
+    int rval = spec->map_func(ds, fd);
+
+    if (special_snowflake) {
+        sigprocmask(SIG_UNBLOCK, &sigmask, NULL);
+        int rst_fd = open("mr.rst", O_CREAT | O_WRONLY, S_IRWXU);
+        pthread_mutex_lock(&reduce_lock);
+        spec->reduce_func(fd_arr, spec->split_num, rst_fd);
+    }
+    _exit(rval);
 }
--- proj2/usr_functions.c
+++ proj2/usr_functions.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,22 +11,36 @@
 /* User-defined map function for the "Letter counter" task.
    This map function is called in a map worker process.
    @param split: The data split that the map function is going to work on.  Note
-   that the file offset of the file descripter split->fd should be set to the
+   that the file offset of the file descriptor split->fd should be set to the
    properly position when this map function is called.
-   @param fd_out: The file descriptor of the itermediate data file output by the
-   map function.
+   @param fd_out: The file descriptor of the intermediate data file output by
+   the map function.
    @ret: 0 on success, -1 on error.
  */
 int letter_counter_map(DATA_SPLIT* split, int fd_out) {
     // add your implementation here ...
-
+    int freq[26] = {0};  // Zero initialize
+    FILE* fp = fdopen(split->fd, "r");
+    FILE* wp = fdopen(fd_out, "w");
+    for (int i = 0; i < split->size; i++) {
+        char c = fgetc(fp);
+        if (isalpha(c)) {
+            c = toupper(c);
+            freq[c - 'A']++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        fprintf(wp, "%c %d\n", 'A' + i, freq[i]);
+    }
+    fflush(wp);
+    fclose(fp);
     return 0;
 }
 
 /* User-defined reduce function for the "Letter counter" task.
    This reduce function is called in a reduce worker process.
    @param p_fd_in: The address of the buffer holding the intermediate data
-   files' file descriptors.  The imtermeidate data files are output by the map
+   files' file descriptors.  The intermediate data files are output by the map
    worker processes, and they are the input for the reduce worker process.
    @param fd_in_num: The number of the intermediate files.
    @param fd_out: The file descriptor of the final result file.
@@ -36,17 +51,34 @@ int letter_counter_map(DATA_SPLIT* split, int fd_out) {
 
 */
 int letter_counter_reduce(int* p_fd_in, int fd_in_num, int fd_out) {
-    // add your implementation here ...
+    int freq[26] = {0};
+    FILE* wp = fdopen(fd_out, "w");
+    for (int i = 0; i < fd_in_num; i++) {
+        lseek(p_fd_in[i], 0, SEEK_SET);
+        FILE* fp = fdopen(p_fd_in[i], "r");
+        while (!feof(fp)) {
+            char c = 0;
+            int num = 0;
+            fscanf(fp, "%c %d\n", &c, &num);
+            freq[c - 'A'] += num;
+        }
+        fclose(fp);
+    }
 
+    for (int i = 0; i < 26; i++) {
+        fprintf(wp, "%c %d\n", 'A' + i, freq[i]);
+    }
+    fclose(wp);
     return 0;
 }
 
 /* User-defined map function for the "Word finder" task.
    This map function is called in a map worker process.
    @param split: The data split that the map function is going to work on.  Note
-   that the file offset of the file descripter split->fd
+   that the file offset of the file descriptor split->fd
    should be set to the properly position when this map function is called.
-   @param fd_out: The file descriptor of the itermediate data file output by the
+   @param fd_out: The file descriptor of the intermediate data file output by
+   the
    map function.
    @ret: 0 on success, -1 on error.
  */
@@ -59,7 +91,7 @@ int word_finder_map(DATA_SPLIT* split, int fd_out) {
 /* User-defined reduce function for the "Word finder" task.
    This reduce function is called in a reduce worker process.
    @param p_fd_in: The address of the buffer holding the intermediate data
-   files' file descriptors.  The imtermeidate data files are output by the map
+   files' file descriptors.  The intermediate data files are output by the map
    worker processes, and they are the input for the reduce worker process.
    @param fd_in_num: The number of the intermediate files.
    @param fd_out: The file descriptor of the final result file.
